0183287b9663418f86eb05a0756053e025c6395c
DomeniGeco <domenigeco@gmail.com>
2026-05-15 17:52:58 +0200

    Update db.py

--- dsite/db.py
+++ dsite/db.py
@@ -1,11 +1,32 @@
+# dsite/db.py
+
+class QuerySet(list):
+    def all(self):
+        return self
+
+    def get(self, id):
+        for obj in self:
+            if getattr(obj, "id", None) == id:
+                return obj
+        return None
+
+
 class Model:
-    _db = []
+    _db = QuerySet()
+    _id_counter = 1
 
     def save(self):
-        self._db.append(self)
+        if not hasattr(self, "id"):
+            self.id = Model._id_counter
+            Model._id_counter += 1
+
+        Model._db.append(self)
+
+    @classmethod
+    def objects(cls):
+        return cls._db
 
 
 class TextColumn:
     def __init__(self, max_length=None):
         self.max_length = max_length
-        self.value = None

9adba49c52c3768b1972449696a7093edd5c5d7f
DomeniGeco <domenigeco@gmail.com>
2026-05-15 17:49:36 +0200

    Add Model and TextColumn classes for database

--- dsite/db.py
+++ dsite/db.py
@@ -1,1 +1,11 @@
+class Model:
+    _db = []
 
+    def save(self):
+        self._db.append(self)
+
+
+class TextColumn:
+    def __init__(self, max_length=None):
+        self.max_length = max_length
+        self.value = None

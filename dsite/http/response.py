1da6eb444eff03e9306a3a8184249946c9cfe0f6
DomeniGeco <domenigeco@gmail.com>
2026-05-15 17:50:59 +0200

    Update response.py

--- dsite/http/response.py
+++ dsite/http/response.py
@@ -1,1 +1,8 @@
+class Response:
+    def __init__(self, content, status=200, headers=None):
+        self.content = content
+        self.status = status
+        self.headers = headers or {}
 
+    def to_http(self):
+        return self.content

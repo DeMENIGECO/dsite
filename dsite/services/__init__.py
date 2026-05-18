d8d43ac61a1de3bc71eeb978ec5f3b99262914aa
DomeniGeco <domenigeco@gmail.com>
2026-05-18 15:05:15 +0200

    Add builtin services to __init__.py

---
+++ dsite/services/__init__.py
@@ -0,0 +1,30 @@
+builtin_services = [
+  # DSite
+  "dsite",
+  
+  #di DSite.http
+  "dsite.http"
+  "dsite.http.request",
+  "dsite.http.response",
+  "dsite.http.router",
+
+  #di Dsite.management
+  "dsite.management"
+  "dsite.management.adminsite",
+  "dsite.management.migrations",
+  "dsite.management.runserver",
+
+  #Di Dsite.services
+  "dsite.services",
+  "dsite.services.management",
+
+  #Altro
+  "dsite.admin"
+  "dsite.adminsite",
+  "dsite.db",
+  "dsite.forms",
+  "dsite.server",
+  "dsite.shortcuts",
+  "dsite.template",
+  "dsite.urls"
+]
